4ba98dde04e8a92d66579237404734a61910739b
amu1uu <muduol@gmail.com>
2026-07-05 13:21:12 +0800

    双指针

---
+++ src/main/java/com/amu1uu/leetcode/array/NO27.java
@@ -0,0 +1,34 @@
+package com.amu1uu.leetcode.array;
+
+/**
+ * @author amu1uu
+ * {@code @date } 2026年07月05日 13:11
+ */
+
+/**
+ * 给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素。元素的顺序可能发生改变。然后返回 nums 中与 val 不同的元素的数量。
+ *
+ * 假设 nums 中不等于 val 的元素数量为 k，要通过此题，您需要执行以下操作：
+ *
+ * 更改 nums 数组，使 nums 的前 k 个元素包含不等于 val 的元素。nums 的其余元素和 nums 的大小并不重要。
+ * 返回 k。
+ */
+
+/**
+ * [0,1,2,2,3,0,4,2] val=2
+ * 0 1 3
+ */
+public class NO27 {
+
+    public int removeElement(int[] nums, int val) {
+        int k = 0;
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[j++] = nums[i];
+            }
+        }
+        return j;
+    }
+
+}
